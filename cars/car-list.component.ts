faf0208d59b3c4994e59f2228507c28359b0df22
Manol Donev <manol.donev@gmail.com>
2018-04-04 16:46:36 +0300

    feat: add support for webpack4/angular6/rxjs6

--- cars/car-list.component.ts
+++ cars/car-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ObservableArray } from "data/observable-array";
 import { RouterExtensions } from "nativescript-angular/router";
 import { ListViewEventData } from "nativescript-ui-listview";
+import { finalize } from "rxjs/operators";
 import { Subscription } from "rxjs/Subscription";
 
 import { Car } from "./shared/car.model";
@@ -42,7 +43,7 @@ export class CarListComponent implements OnInit, OnDestroy {
         *************************************************************/
         if (!this._dataSubscription) {
             this._dataSubscription = this._carService.load()
-                .finally(() => this._isLoading = false)
+                .pipe(finalize(() => this._isLoading = false))
                 .subscribe((cars: Array<Car>) => {
                     this._cars = new ObservableArray(cars);
                     this._isLoading = false;
